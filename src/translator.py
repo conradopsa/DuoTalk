4029f15d3343946484739d1dae224352f696d52d
Conrado Almeida <conradopsa@gmail.com>
2025-06-04 20:02:37 -0300

    adding retry backoffice to the translation

--- src/translator.py
+++ src/translator.py
@@ -1,8 +1,7 @@
 from deep_translator import GoogleTranslator
+from backoff import on_exception, expo
 
+@on_exception(expo, Exception, max_tries=5, base=1, max_value=60)
 def translate(text, src='en', dest='pt'):
-    try:
-        return GoogleTranslator(source=src, target=dest).translate(text)
-    except Exception as e:
-        print(f"[translation error] {e}")
-        return "[translation error]"
+    return GoogleTranslator(source=src, target=dest).translate(text)
+    

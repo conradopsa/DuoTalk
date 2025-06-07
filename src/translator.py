6289dab4391d39879a201d54a06132f07a1effbe
Conrado Almeida <conradopsa@gmail.com>
2025-06-06 22:37:07 -0300

    adding translator consitency on first letter

--- src/translator.py
+++ src/translator.py
@@ -3,5 +3,12 @@ from backoff import on_exception, expo
 
 @on_exception(expo, Exception, max_tries=5, base=1, max_value=60)
 def translate(text, src='en', dest='pt'):
-    return GoogleTranslator(source=src, target=dest).translate(text)
+    startsWithLower = text[0].islower()
+    
+    translated = GoogleTranslator(source=src, target=dest).translate(text)
+    
+    if startsWithLower:
+        translated[0] = translated[0].lower()
+        
+    return 
     
